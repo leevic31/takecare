d2ebdb070b04a7d5b7cc17aea96b2d634b47604e
leevic31 <leevic31@gmail.com>
2024-05-07 14:02:56 -0400

    added date to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_171043) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_180157) do
   create_table "bookings", force: :cascade do |t|
     t.boolean "available", default: true
     t.datetime "start_time"
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
   end
 
   create_table "organizations", force: :cascade do |t|
