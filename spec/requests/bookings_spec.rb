3e8ea5ba70206560baf573465fc149db29dcbfae
leevic31 <leevic31@gmail.com>
2024-05-06 23:24:01 -0400

    wip specs for days and bookings

--- spec/requests/bookings_spec.rb
+++ spec/requests/bookings_spec.rb
@@ -4,14 +4,7 @@ RSpec.describe "BookingsController", type: :request do
     describe "GET /bookings" do
         it "gets Bookings" do
             get bookings_path
-            expect(response.content_type).to eq("application/json; charset=utf-8")        
-        end
-    end
-
-    describe "POST /bookings" do
-        it "creates a Booking" do
-            post bookings_path
-            expect()
+            expect(response).to have_http_status(:success)        
         end
     end
 end
--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "OrganizationsController", type: :request do
     describe "GET /organizations" do
         it "gets Organizations" do
             get organizations_path
-            expect(response).to have_http_status(:ok)
+            expect(response).to have_http_status(:success)
         end
     end
 end
