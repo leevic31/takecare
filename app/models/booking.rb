26fb1dfa051163c3150f047dbd45118c34a1d51c
leevic31 <leevic31@gmail.com>
2024-06-18 15:11:52 -0400

    implemented book_appointment method

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,9 @@
 class Booking < ApplicationRecord
     validates :date, :start_time, :end_time, :available, presence: true
+
+    def book_appointment
+        if self.available
+            self.update(available: false)
+        end
+    end
 end
--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class BookingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @booking = Booking.new(
+      available: true,
+      start_time: "2024-05-04 13:45:12",
+      end_time: "2024-05-04 13:45:12",
+      date: "2024-05-04"
+    )
+  end
+
+  test "should be valid" do
+    assert @booking.valid?
+  end
+
+  test "available should be present" do
+    @booking.available = nil
+    assert_not @booking.valid?
+  end
+
+  test "start time should be present" do
+    @booking.start_time = nil
+    assert_not @booking.valid?
+  end
+
+  test "end time should be present" do
+    @booking.end_time = nil
+    assert_not @booking.valid?
+  end
+
+  test "date should be present" do
+    @booking.date = nil
+    assert_not @booking.valid?
+  end
+
+  test "book_appointment should change availablity of booking from true to false" do
+    @booking.book_appointment
+    assert_equal false, @booking.available
+  end
 end
