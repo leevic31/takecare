b105ede758a56505a746f8853c57280c49c5b055
leevic31 <leevic31@gmail.com>
2024-07-02 00:16:24 -0400

    created policies for orgs and services

---
+++ app/policies/organization_policy.rb
@@ -0,0 +1,31 @@
+class OrganizationPolicy < ApplicationPolicy
+  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
+  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
+  # In most cases the behavior will be identical, but if updating existing
+  # code, beware of possible changes to the ancestors:
+  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      if user.has_role?(:admin)
+        scope.all
+      end
+    end
+  end
+
+  def index?
+    user.has_role?(:admin)
+  end
+
+  def create?
+    user.has_role?(:admin)
+  end
+
+  def update?
+    user.has_role?(:admin)
+  end
+
+  def destroy?
+    user.has_role?(:admin)
+  end
+end
