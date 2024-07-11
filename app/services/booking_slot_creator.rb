479207cfeaad043b8f1625ee6f139e940801bdf0
leevic31 <leevic31@gmail.com>
2024-07-10 21:51:12 -0400

    Refactored booking slot creator service

--- app/services/booking_slot_creator.rb
+++ app/services/booking_slot_creator.rb
@@ -1,16 +1,14 @@
 class BookingSlotCreator
-    def self.create_booking_slots
-        AvailabilityBlock.all.each do |block|
-            current_time = block.start_time
-            while current_time < block.end_time
-                # Create booking slot for 1 hour
-                booking = block.bookings.create!(
-                    start_time: current_time,
-                    end_time: current_time + 1.hour,
-                    user: block.user
-                )
-                current_time += 1.hour
-            end
+    def self.create_booking_slots(availability_block)
+        current_time = availability_block.start_time
+        while current_time < availability_block.end_time
+            # Create booking slot for 1 hour
+            booking = availability_block.bookings.create!(
+                start_time: current_time,
+                end_time: current_time + 1.hour,
+                user: availability_block.user
+            )
+            current_time += 1.hour
         end
     end
 end
