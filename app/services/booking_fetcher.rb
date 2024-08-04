a8381f9c9f843d0ed82ff858ca313ae2bbc61044
leevic31 <leevic31@gmail.com>
2024-08-03 23:55:04 -0400

    refactored booking fetcher

--- app/services/booking_fetcher.rb
+++ app/services/booking_fetcher.rb
@@ -1,25 +1,23 @@
 class BookingFetcher
-    def initialize(date:, hour:)
-        @date = date
-        @hour = hour.to_i
+  def initialize(date:)
+    @date = Date.parse(date)
+    @bookings_by_hour = {}
+  end
+
+  def call
+    fetch_bookings
+  end
+
+  private
+
+  def fetch_bookings
+    (8..20).each do |hour|
+      start_hour, end_hour = @date.to_datetime.change(hour: hour), @date.to_datetime.change(hour: hour + 1)
+      bookings = Booking.where('start_time >= ? AND start_time < ?', start_hour, end_hour)
+      @bookings_by_hour[hour] = bookings 
     end
 
-    def call
-        validate_hour
-        fetch_bookings
-    end
-
-    private
-
-    def validate_hour
-        if @hour < 0 || @hour > 23
-            raise ArgumentError, 'Invalid hour parameter. Must be between 0 and 23.'
-        end
-    end
-
-    def fetch_bookings
-        start_time = Time.zone.parse("#{@date} #{@hour}:00:00")
-        end_time = start_time + 1.hour
-        Booking.where("start_time >= ? AND start_time < ?", start_time, end_time)
-    end
+    @bookings_by_hour
+  end
 end
+
