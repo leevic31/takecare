e0444777b1928260c5dba627a98edd7609d3c8e2
leevic31 <leevic31@gmail.com>
2024-09-13 15:34:10 -0400

    Changed booking fetcher to only grab available bookings

--- app/services/booking_fetcher.rb
+++ app/services/booking_fetcher.rb
@@ -10,7 +10,7 @@ class BookingFetcher
   private
 
   def fetch_bookings
-    Booking.includes(:staff_member).where(start_time: @date.beginning_of_day..@date.end_of_day).order(:start_time)
+    Booking.includes(:staff_member).where(start_time: @date.beginning_of_day..@date.end_of_day, available: true).order(:start_time)
   end
 end
 
